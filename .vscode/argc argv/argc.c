40440c7128cd16a4697a733700539b35c8559a86
tamerbeshay-code <tamer.beshay@gmail.com>
2026-03-09 23:36:21 +0000

    b

---
+++ .vscode/argc argv/argc.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   argc.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TBeshay <tamer_karam@hotmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 19:13:40 by TBeshay           #+#    #+#             */
+/*   Updated: 2026/03/09 23:35:23 by TBeshay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc,	char* argv[])
+{
+
+int i, sum = 0;
+	
+printf("argc =%d\n",argc );
+
+
+printf("argv=\n");
+if (argc >1)
+{
+for(i=0 ;  i < argc ; i++)
+{
+printf("argv[%d] = %s\n", i, argv[i]);
+sum+= atoi( argv [i]);
+}
+}
+return(0);
+}
+
