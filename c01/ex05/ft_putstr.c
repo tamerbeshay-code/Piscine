19d362ae97734ec9eab5c5654f5061b767217d59
tamerbeshay-code <tamer.beshay@gmail.com>
2026-03-11 01:28:51 +0000

    m

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: TBeshay <tamer_karam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 00:52:47 by TBeshay           #+#    #+#             */
-/*   Updated: 2026/03/11 01:16:26 by TBeshay          ###   ########.fr       */
+/*   Updated: 2026/03/11 01:22:39 by TBeshay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	ft_putstr(char *str)
 
 	i = 0;
 
-	// While the string doesn't reach the null
+	// While the string doesn't reach the null+++++++++
 	while (str[i])
 	{
-		// Each Char passed by the index "i" will be printed
+		// Char passed by the index "i" will be printed
 		write(1, &str[i], 1);
 		i++;
 	}	
